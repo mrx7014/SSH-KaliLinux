c6549da0b82afca744479b2f057ad3572832f1a5
Kareem Ebrahem <90919499+mrx7014@users.noreply.github.com>
2023-04-26 17:56:17 +0000

    Add files via upload

---
+++ linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo " __  __ _____  __   ________ ___  __ _  _  "
+echo "|  \/  |  __ \ \ \ / /____  / _ \/_ | || | "
+echo "| \  / | |__) | \ V /    / / | | || | || | "
+echo "| |\/| |  _  /   > <    / /| | | || |__   _|"
+echo "| |  | | | \ \  / . \  / / | |_| || |  | |  "
+echo "|_|  |_|_|  \_\/_/ \_\/_/   \___/ |_|  |_|"
+
+sleep 2
+
+echo "Welcome At Linux Setup"
+
+sleep 2
+
+echo "Script By mrx7014"
+
+sleep 2
+
+echo "Join our channel t.me/goosetech7014"
+
+sleep 1
+
+sudo apt update && sudo apt upgrade && sudo apt install openssh && ssh root@segfault.net
---
+++ termux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo " __  __ _____  __   ________ ___  __ _  _  "
+echo "|  \/  |  __ \ \ \ / /____  / _ \/_ | || | "
+echo "| \  / | |__) | \ V /    / / | | || | || | "
+echo "| |\/| |  _  /   > <    / /| | | || |__   _|"
+echo "| |  | | | \ \  / . \  / / | |_| || |  | |  "
+echo "|_|  |_|_|  \_\/_/ \_\/_/   \___/ |_|  |_|"
+
+sleep 2
+
+echo "Welcome At Termux Setup"
+
+sleep 2
+
+echo "Script By mrx7014"
+
+sleep 2
+
+echo "Join our channel t.me/goosetech7014"
+
+sleep 1
+
+pkg update && pkg upgrade && pkg install openssh && ssh root@segfaul.net
